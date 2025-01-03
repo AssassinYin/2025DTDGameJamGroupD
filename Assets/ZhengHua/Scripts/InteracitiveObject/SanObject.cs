f7df0c77869b147e0178354d563c93345e3bff94
zhenghua <aaazzzz168@gmail.com>
2025-01-04 01:03:30 +0800

    feat: 建構可互動物件相關雛形

---
+++ Assets/ZhengHua/Scripts/InteracitiveObject/SanObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ZhengHua
+{
+    /// <summary>
+    /// San值扣除物件
+    /// </summary>
+    public abstract class SanObject : InteracitiveObject
+    {
+        /// <summary>
+        /// 碰觸後須扣除的San值
+        /// </summary>
+        [SerializeField]
+        private float sanValue = 0f;
+        public override void Execute()
+        {
+            /// 呼叫玩家數值管理器的扣除San值方法
+            Debug.Log("扣除San值: " + sanValue);
+        }
+    }
+}
