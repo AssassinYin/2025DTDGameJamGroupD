18404eafcd776663cb8916f4f1e390df8934a7a3
YouZhengHua <42639936+YouZhengHua@users.noreply.github.com>
2025-01-04 18:03:06 +0800

    新增 PlayerManager 與 無敵狀態設置

---
+++ Assets/ZhengHua/Scripts/PlayerManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.Events;
+
+namespace ZhengHua
+{
+    public class PlayerManager
+    {
+        private static PlayerManager instance;
+        private static readonly object _lock = new object();
+
+        public static PlayerManager Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new PlayerManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public UnityEvent OnRoundEnd;
+        private int invincibleTurns = 0;
+
+        private PlayerManager()
+        {
+            OnRoundEnd = new UnityEvent();
+            invincibleTurns = 0;
+            OnRoundEnd.AddListener(RoundEnd);
+        }
+
+        private void RoundEnd()
+        {
+            if(invincibleTurns > 0)
+            {
+                invincibleTurns--;
+            }
+        }
+
+        /// <summary>
+        /// 是否為無敵狀態
+        /// </summary>
+        public bool IsInvinciable { get => invincibleTurns > 0; }
+
+        /// <summary>
+        /// 進入無敵狀態
+        /// </summary>
+        /// <param name="turns"></param>
+        public void EnterInvincible(int turns = 2)
+        {
+            invincibleTurns = turns;
+        }
+    }
+}
