95b0ea561c9303e8c5808b16250a825fee6f7ba5
YouZhengHua <42639936+YouZhengHua@users.noreply.github.com>
2025-01-05 15:06:47 +0800

    fix: 修正重新開始時重力未回歸正常的問題

--- Assets/ZhXun/Script/GameOverUI.cs
+++ Assets/ZhXun/Script/GameOverUI.cs
@@ -6,10 +6,12 @@ namespace ZhXun
     public class GameOverUI : MonoBehaviour
     {
         [SerializeField] GameObject gameOverUI;
+        Vector2 defaultGravity; // 默認重力方向
 
         void Start()
         {
             GameOverManager.Instance.OnGameOver.AddListener(OpenGameOverUI);
+            defaultGravity = Physics2D.gravity;
         }
 
         void OpenGameOverUI()
@@ -19,6 +21,7 @@ namespace ZhXun
 
         public void Restart()
         {
+            Physics2D.gravity = defaultGravity;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
